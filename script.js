8a1c2095fd77343bb26a56e3cd3572834d2972bf
Vinicius Avelar <avelarvnc@gmail.com>
2023-02-27 14:59:20 -0300

    main-content

---
+++ script.js
@@ -0,0 +1,16 @@
+function controleMenu() {
+          
+    var x = document.getElementById("menu-navegacao");
+    
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } 
+    else {
+        x.style.display = "flex";
+    }
+}
+
+function fecharMenu(){
+    var x = document.getElementById("menu-navegacao");
+    x.style.display = "none";
+}
